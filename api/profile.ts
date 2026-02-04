4963ea191394b4634c3ccfd9f1eaf05b9aa2a647
llfisher932 <llfisher932@gmail.com>
2026-02-04 16:20:43 -0500

    supabase

--- api/profile.ts
+++ api/profile.ts
@@ -1,17 +1,19 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import supabase from "../src/supabase";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  // Only allow POST
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "POST") {
     res.setHeader("Allow", "POST");
     return res.status(405).json({ error: "Method not allowed" });
   }
 
   // Parse JSON body (Vercel parses it automatically if sent as JSON)
-  const data = req.body;
+  let games = await supabase.from("matches").select();
+  const data = games;
 
   console.log("Body received:", data);
 
   // Echo the data back
   res.status(200).json(data);
 }
+//St8rW8rs!W8rs!!
