8295fdb01d0951d55aeafcc53f76b187ef704494
llfisher932 <llfisher932@gmail.com>
2026-02-04 13:18:41 -0500

    losing hope

--- api/profile.ts
+++ api/profile.ts
@@ -3,12 +3,16 @@ import cors from "cors";
 
 const app = express();
 
+// Enable CORS
 app.use(cors({ origin: "*" }));
+
+// Parse JSON body
 app.use(express.json());
 
+// POST to the **root** of this API file
 app.post("/", (req, res) => {
   console.log("Body received:", req.body);
-  res.json(req.body);
+  res.json(req.body); // echoes the request
 });
 
 export default app;
