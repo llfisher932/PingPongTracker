ce85eab34aab111de75df08125c9b516d1456c1d
llfisher932 <llfisher932@gmail.com>
2026-02-04 13:14:58 -0500

    done maybe

--- api/profile.ts
+++ api/profile.ts
@@ -1,3 +1,14 @@
-import app from "../src/server";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  console.log("Body received:", req.body);
+  res.json(req.body);
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import express from "express";
-import cors from "cors";
-const app = express();
-app.use(
-  cors({
-    origin: "*",
-  }),
-);
-app.use(express.json());
-// Define your routes
-app.post("/", (req, res, _next) => {
-  console.log(req.body);
-  res.json(req.body);
-});
-export default app;
+// import express from "express";
+// import cors from "cors";
+// const app = express();
+// app.use(
+//   cors({
+//     origin: "*",
+//   }),
+// );
+// app.use(express.json());
+// // Define your routes
+// app.post("/", (req, res, _next) => {
+//   console.log(req.body);
+//   res.json(req.body);
+// });
+// export default app;
