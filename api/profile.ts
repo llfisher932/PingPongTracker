be38f59d4dd8b9dbe92e591b48c3d2086e22c296
llfisher932 <llfisher932@gmail.com>
2026-02-04 16:37:18 -0500

    done

--- api/profile.ts
+++ api/profile.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import supabase from "../src/supabase";
+import supabase from "./supabase";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "POST") {
